ad21e652b0f381bd53656dc3527a1c94dae3a621
mas400 <mas400@dcs27-mdt.dbmi.pitt.edu>
2016-02-19 14:01:14 -0500

    nick should update to python 3

--- base_output_translator.py
+++ base_output_translator.py
@@ -4,7 +4,7 @@ import h5py
 def hdf5_to_dataframe(filename):
     hdf5_file = h5py.File('output.allegheny.county_age.race.gender.location.hdf5', 'r')
 
-    dataset = hdf5_file.keys()[0]
+    dataset = list(hdf5_file.keys())[0]
     hdf5_file.close()
 
     df = pd.read_hdf(filename, key=dataset)
